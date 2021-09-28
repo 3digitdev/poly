c6a8b1380c00ab5a7e3cbdc4debf5c7cc6881e6b
Max Bussiere <max@3digit.dev>
2021-09-28 16:48:34 -0500

    working copy of install file

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@ git clone https://github.com/3digitdev/poly.git ./poly
 
 if [ -f "/usr/bin/poly" ]; then
   rm -rf poly
-  >&2 echo "ERROR: /usr/bin/poly already exists; exiting"
+  >&2 echo "ERROR: /usr/bin/poly already exists - exiting"
   exit 1
 else
   cp poly/poly /usr/bin/poly
   rm -rf poly/
-  rm install.sh
   exit 0
 fi
