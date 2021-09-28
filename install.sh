9c57a89b57f9a134e85bcb00f8f8d236bec7b08a
Max Bussiere <max@3digit.dev>
2021-09-28 16:43:27 -0500

    cleanup?

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 git clone https://github.com/3digitdev/poly.git ./poly
-cd poly
 
 if [ -f "/usr/bin/poly"]; then
-  cd .. && rm -rf ./poly
+  rm -rf poly
   >&2 echo "ERROR: /usr/bin/poly already exists; exiting"
-  exit(1)
+  exit 1
 else
-  cp poly /usr/bin/poly
+  cp poly/poly /usr/bin/poly
+  rm -rf poly/
+  rm install.sh
+  exit 0
 fi
