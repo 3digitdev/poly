ad935b9e4d13875b13156c9be499904463721f16
Max Bussiere <max@3digit.dev>
2021-09-28 16:43:55 -0500

    fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/3digitdev/poly.git ./poly
 
-if [ -f "/usr/bin/poly"]; then
+if [ -f "/usr/bin/poly" ]; then
   rm -rf poly
   >&2 echo "ERROR: /usr/bin/poly already exists; exiting"
   exit 1
