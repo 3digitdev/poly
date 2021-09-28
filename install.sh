26d874a16f750ad44ba347a259c36f149fa2fd11
Max Bussiere <max@3digit.dev>
2021-09-28 16:34:51 -0500

    install script first try

---
+++ install.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/3digitdev/poly.git .
+cd poly
+
+if [ -f "/usr/bin/poly"]; then
+  cd .. && rm -rf ./poly
+  >&2 echo "ERROR: /usr/bin/poly already exists; exiting"
+  exit(1)
+else
+  cp poly /usr/bin/poly
+fi
